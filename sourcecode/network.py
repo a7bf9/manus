a426109871ce5bd30b012088ea19532f3ff26fa9
lloydmeins <github@lloydmeins.de>
2019-07-11 21:18:59 +0200

    Add first data and structure the code for the neural network

---
+++ sourcecode/network.py
@@ -0,0 +1,52 @@
+import numpy
+
+
+class neuralNetwork:
+    """Neural Network class"""
+
+    def __init__(self, inputnodes, hiddennodes, outputnodes, learningrate):
+        self.inodes = inputnodes
+        self.hnodes = hiddennodes
+        self.onodes = learningrate
+        self.lr = learningrate
+
+        self.wih = numpy.random.normal(0.0, pow(self.hnodes, -0.5), (self.hnodes, self.inodes))
+        self.who = numpy.random.normal(0.0, pow(self.onodes, -0.5), (self.onodes, self.hnodes))
+
+        self.activation_function = lambda x: scipy.special.expit(x)
+
+    # training the network
+    def train(self, inputs_list, targets_list):
+        inputs = numpy.array(inputs_list, ndmin=2).T
+        targets = numpy.array(targets_list, ndmin=2).T
+
+        hidden_inputs = numpy.dot(self.wih, inputs)
+        hidden_outputs = self.activation_function(hidden_inputs)
+
+        final_inputs = numpy.dot(self.who, hidden_outputs)
+        final_outputs = self.activation_function(final_inputs)
+
+        output_errors = targets - final_outputs
+        hidden_errors = numpy.dot(self.who.T, output_errors)
+
+        self.who += self.lr * numpy.dot((output_errors * final_outputs * (1.0 - final_outputs)), numpy.transpose(inputs))
+
+    # query the network
+    def query(self, inputs_list):
+        inputs = numpy,array(inputs_list, ndmin=2).T
+
+        hidden_inputs = numpy.dot(self.wih, inputs)
+        hidden_outputs = self.activation_function(hidden_inputs)
+
+        final_inputs = numpy.dot(self.who, hidden_outputs)
+        final_outputs = self.activation_function(final_inputs)
+
+        return final_outputs
+
+# Create a sample object
+input_nodes = 3
+hidden_nodes = 3
+output_nodes = 3
+learnrate = 0.3
+
+n = neuralNetwork(input_nodes, hidden_nodes, output_nodes, learnrate)
